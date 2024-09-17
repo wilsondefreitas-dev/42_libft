6f785b30c3b09e76ce1edd9915e70616dd02b46b
wilsondefreitas-dev <wilsondefreitas92@gmail.com>
2024-09-17 06:37:01 -0300

    feat: Update read.me and add test file

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # LIBFT
 
 run `make`
+
+run `gcc -o main main.c libft.a`
+
+run `./main`
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "libft.h"
+
+int main(void){
+    const char* str = "Testing";
+
+    printf("size of the string '%s': %d\n", str, ft_strlen(str));
+}
