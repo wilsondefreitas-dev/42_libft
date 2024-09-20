0aaa03c33212b45a874d70634ad6f853e31806c7
wilsondefreitas-dev <wilsondefreitas92@gmail.com>
2024-09-20 01:55:22 -0300

    fix: Improve ft_strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -1,9 +1,9 @@
 int ft_strcmp(const char *str1, const char *str2)
 {
     int i = 0;
-    while (str1[i] || str2[i])
+    while (str1[i] != '\0' || str2[i] != '\0')
     {
-        if (str1[i] != str2[i] || !str1[i] || !str2[i])
+        if (str1[i] != str2[i] || str1[i] == '\0' || str2[i] == '\0')
             return 0;
 
         i++;
