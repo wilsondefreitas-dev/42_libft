f73868ffad2f37c83da474e4f7e2a63dfcd0435f
wilsondefreitas-dev <wilsondefreitas92@gmail.com>
2024-09-20 02:59:39 -0300

    fix: Improve ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,6 +1,8 @@
-int ft_strlen(const char* str){
+int ft_strlen(const char *str)
+{
     unsigned int i = 0;
-    while(str[i]){
+    while (str[i] != '\0')
+    {
         i++;
     }
     return i;
